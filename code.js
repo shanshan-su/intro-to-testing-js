72c12ba77d4113f0732ea76e629b5c56ccdcab04
Shanshan Su <shanshansu36@gmail.com>
2021-02-09 14:00:48 -0600

    feat: finished exercise 6 refactoring sayHello function

--- code.js
+++ code.js
@@ -7,12 +7,17 @@ const helloWorld = function() {
     return "Hello, World!";
 }
 
+// function sayHello(input) {
+//     if (input === "Alex") {
+//         return "Hello, Alex!";
+//     } else if (input === "Pat") {
+//         return "Hello, Pat!";
+//     } else {
+//         return "Hello, Jane!";
+//     }
+// }
+
+// Exercise #6 Implement the refactor!
 function sayHello(input) {
-    if (input === "Alex") {
-        return "Hello, Alex!";
-    } else if (input === "Pat") {
-        return "Hello, Pat!";
-    } else {
-        return "Hello, Jane!";
-    }
+    return "Hello, " + input + "!";
 }
